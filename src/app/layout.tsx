6ce4388c045a6e7d09538cd5b06a1259ddc2dae3
kimhwanhoon <kimhwanhoon@outlook.com>
2024-12-05 17:17:09 +0100

    metadata modulized

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,11 @@
-import type { Metadata } from 'next';
-// import localFont from "next/font/local";
+import { metadata } from '@/metadata/home/metadata';
 import './globals.css';
+
 import { Header } from '@/components/header/header';
 import { Footer } from '@/components/footer/footer';
 import { Suspense } from 'react';
 
-export const metadata: Metadata = {
-  title: 'Diary App',
-  description: 'Diary App',
-};
+export { metadata };
 
 export default function RootLayout({
   children,
